cf5f99498e61f0223a7b524aabcbaf75105f9532
unknown <kennynnopu@gmail.com>
2020-10-06 14:58:50 +0100

    social app

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+const config = {
+  apiKey: "AIzaSyClu5hWNXuhuX2ttO6l2zbT7qNn7jMTD4A",
+  authDomain: "socialape-8b19a.firebaseapp.com",
+  databaseURL: "https://socialape-8b19a.firebaseio.com",
+  projectId: "socialape-8b19a",
+  storageBucket: "socialape-8b19a.appspot.com",
+  messagingSenderId: "612911522222",
+  appId: "1:612911522222:web:b717bbc09148b863b9e83f",
+  measurementId: "G-TNGX9WV7SH",
+};
+
+firebase.initializeApp(config);
+export default firebase;
